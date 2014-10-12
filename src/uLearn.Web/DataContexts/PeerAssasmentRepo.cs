d5efc0c6b3b3f103298baeb6dcefbcc5f65c5a62
denis boginich <denis.boginich@skbkontur.ru>
2014-10-12 21:20:02 +0600

    Репозиторий для PeerAssasment.

---
+++ src/uLearn.Web/DataContexts/PeerAssasmentRepo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using uLearn.Web.Models;
+
+namespace uLearn.Web.DataContexts
+{
+    public class PeerAssasmentRepo
+    {
+        private readonly ULearnDb db;
+
+        public PeerAssasmentRepo()
+            : this(new ULearnDb())
+        {
+        }
+
+        private PeerAssasmentRepo(ULearnDb db)
+        {
+            this.db = db;
+        }
+
+        public async Task<PeerAssasmentAnswer> AddUserProposition(string courseId, string peerAssasemntId, string userId, string text, DateTime time)
+        {
+            var peerAssasmentAnswer = new PeerAssasmentAnswer
+            {
+                CourseId = courseId,
+                Text = text,
+                Timestamp = time,
+                UserId = userId,
+                SlideId = peerAssasemntId
+            };
+            db.PeerAssasmentAnswers.Add(peerAssasmentAnswer);
+            await db.SaveChangesAsync();
+            return peerAssasmentAnswer;
+        }
+
+        public string GetUserProposition(string courseId, string peerAssasemntId, string userId)
+        {
+            var answer = db.PeerAssasmentAnswers
+                .Where(x => x.CourseId == courseId && x.UserId == userId && x.SlideId == peerAssasemntId)
+                .OrderByDescending(x => x.Timestamp)
+                .FirstOrDefault();
+            return answer == null ? "" : answer.Text;
+        }
+    }
+}
---
+++ src/uLearn.Web/Models/PeerAssasmentAnswer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace uLearn.Web.Models
+{
+    public class PeerAssasmentAnswer
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(64)]
+        public string CourseId { get; set; }
+
+        [Required]
+        [StringLength(64)]
+        public string SlideId { get; set; }
+
+        [StringLength(64)]
+        public string UserId { get; set; }
+
+        [Required]
+        [StringLength(4096)]
+        public string Text { get; set; }
+
+        [Required]
+        public DateTime Timestamp { get; set; }
+    }
+}
