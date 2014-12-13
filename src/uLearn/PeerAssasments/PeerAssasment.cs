57480a490ad5cc7fed768023685f423ba7dc055d
Denis Boginich <denis.boginich@gmail.com>
2014-12-13 22:52:27 +0500

    PeerAssasmentSldie и Loader для него.

---
+++ src/uLearn/PeerAssasments/PeerAssasment.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace uLearn.PeerAssasments
+{
+    [XmlRoot("peerAssasment", IsNullable = false)]
+    public class PeerAssasment
+    {
+        [XmlAttribute("id")]
+        public string Id;
+
+        [XmlElement("title")]
+        public string Title;
+
+        [XmlElement("proposition")]
+        public string Proposition;
+
+        [XmlElement("step")]
+        public PeerAssasmentStep[] Steps;
+    }
+
+    public class PeerAssasmentStep
+    {
+        [XmlAttribute("type")]
+        public PeerAssasmentStepType StepType { get; set; }
+
+        [XmlElement("deadline")]
+        public DateTime? Deadline { get; set; }
+    }
+
+    public enum PeerAssasmentStepType
+    {
+        Undefined,
+        PropositionWriting,
+        Review,
+        Observe
+    }
+}
---
+++ src/uLearn/PeerAssasments/PeerAssasmentSlideLoader.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+
+namespace uLearn.PeerAssasments
+{
+    public class PeerAssasmentSlideLoader : ISlideLoader
+    {
+        public string Extension
+        {
+            get { return "pa"; }
+        }
+
+        public Slide Load(FileInfo file, string unitName, int slideIndex)
+        {
+            var peerAssasment = file.DeserializeXml<PeerAssasment>();
+            var slideInfo = new SlideInfo(unitName, file, slideIndex);
+            return new PeerAssasmentSlide(new SlideBlock[0], slideInfo, peerAssasment);
+        }
+    }
+}
