02384f2aa0e9c94d3c4ac00963fad7cc9d64c09d
Denis Boginich <denis.boginich@gmail.com>
2014-12-13 23:17:43 +0500

    PeerAssasementController

---
+++ src/uLearn.Web/Controllers/PeerAssasmentController.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using uLearn.PeerAssasments;
+using uLearn.Web.DataContexts.PeerAssasmentRepository;
+using uLearn.Web.Models.PeerAssasmentModels;
+
+namespace uLearn.Web.Controllers
+{
+    public class PeerAssasmentController : Controller
+    {
+        [Authorize]
+        public ActionResult Run(string courseId, PeerAssasment peerAssasment)
+        {
+            var userId = User.Identity.GetUserId();
+            var slideId = peerAssasment.Id;
+
+            var answer = answerRepository.GetOrCreate(new AnswerId
+            {
+                UserId = userId,
+                CourseId = courseId,
+                SlideId = slideId
+            });
+
+            return View(answer);
+        }
+
+        private readonly PeerAsssasmentAnswerRepository answerRepository = new PeerAsssasmentAnswerRepository();
+    }
+}
