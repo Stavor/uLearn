0a58991a3fd2b5e82ceafa352aff4e56dd1d4152
Denis Boginich <denis.boginich@gmail.com>
2015-02-22 22:36:10 +0500

    добавил view для оценок

--- src/uLearn.Web/Models/PeerAssasmentModels/MarkModel.cs
+++ src/uLearn.Web/Models/PeerAssasmentModels/MarkModel.cs
@@ -4,5 +4,9 @@
     {
         public string Mark { get; set; }
         public string Criterion { get; set; }
+
+        public int MinMark { get; set; }
+        public int MaxMark { get; set; }
+
     }
 }
